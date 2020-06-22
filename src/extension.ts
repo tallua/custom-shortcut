abae096ecfa964087597295ae12c6feb3b1b3173
tallua <talluay@gmail.com>
2020-06-22 20:46:52 +0900

    add contributes/dummy codes

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "custom-shortcut" is now active!');
+	console.log('custom-shortcut is activating...');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('custom-shortcut.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from custom-shortcut!');
+	let refreshCommand = vscode.commands.registerCommand('custom-shortcut.refresh', () => {
+		vscode.window.showInformationMessage('Custom Shortcut : Refresh');
 	});
+	context.subscriptions.push(refreshCommand);
 
-	context.subscriptions.push(disposable);
+	let configCommand = vscode.commands.registerCommand('custom-shortcut.open-config', () => {
+		vscode.window.showInformationMessage('Custom Shortcut : Open  Config');
+	});
+	context.subscriptions.push(configCommand);
+
+	console.log('custom-shortcut is activated');
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
