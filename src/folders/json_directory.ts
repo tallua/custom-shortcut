8db0f979e044eff2faebf8679fdaeaed4ba1983b
tallua <talluay@gmail.com>
2020-07-18 19:08:55 +0900

    re-label json root directory with file name

--- src/folders/json_directory.ts
+++ src/folders/json_directory.ts
@@ -6,14 +6,17 @@ import { Shortcut } from '../shortcut_provider';
 import { JsonShortcut } from '../shortcuts/json_shortcut';
 import { JsonDirectoryScehma, JsonFileSchema } from '../common/json_schema'
 
-
 export class JsonDirectory implements Shortcut {
     public readonly collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
     public readonly label : string | undefined;
 
-    private constructor(private readonly json : JsonDirectoryScehma) {
+    private constructor(private readonly json : JsonDirectoryScehma, label? : string) {
         console.debug('JsonDirectory : creating : ' + json.name);
-        this.label = json.name;
+        if(label) {
+            this.label = label;
+        } else {
+            this.label = json.name;
+        }
     }
 
     isFolderType() : boolean {
@@ -47,6 +50,6 @@ export class JsonDirectory implements Shortcut {
     }
 
     private static createRoot(json : JsonFileSchema, label : string) : JsonDirectory {
-        return new JsonDirectory(json.roots.bookmark_bar);
+        return new JsonDirectory(json.roots.bookmark_bar, label);
     }
 }
